b5b62235e6457f549b802fe0800d79cb00476ad4
Alan Jeeves <runawaycoin@gmail.com>
2022-04-29 20:03:30 +0100

    clear, fresh btns added, merge output

--- Resources/js/htmlDescriptor.js
+++ Resources/js/htmlDescriptor.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+
+
   let prev = null;
+
   Array.from(document.getElementsByTagName('article')).reverse().forEach(function (article) {
     const dedupId = article.dataset.dedupId;
     if (dedupId === prev) {
@@ -7,4 +11,54 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     prev = dedupId;
   });
+
+
+  let lastId = Array.from(document.getElementsByTagName('article')).reverse()[0].dataset.dedupId;
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  let hideFrom = params.s;
+
+  document.getElementById('clearBtn').href = "?s=" + lastId;
+  document.getElementById('refreshBtn').href = "?s=" + hideFrom;
+
+
+
+  let hideRest = false;
+  Array.from(document.getElementsByTagName('article')).reverse().forEach(function (article) {
+    const dedupId = article.dataset.dedupId;
+    if (hideRest || dedupId === hideFrom) {
+      article.parentElement.removeChild(article);
+      hideRest = true;
+    }
+  });
+
+
+
+
+  // AJ - so dumps from same dump call are put together
+  prev = null;
+  let prevParent = null;
+  Array.from(document.getElementsByTagName('p')).reverse().forEach(function (article) {
+    const dedupId = article.dataset.dedupId;
+    if (dedupId === prev) {
+      article.classList.add('hidden');
+      // move the pre
+
+      article.parentElement.parentElement.parentElement.removeChild(article.parentElement.parentElement);
+
+      prevParent.appendChild(  article.parentElement.getElementsByTagName('pre')[0]    );
+
+
+    } else {
+      prevParent = article.parentElement;
+    }
+    prev = dedupId;
+  });
+
+
+  document.body.style.visibility = 'visible';
+
 });
