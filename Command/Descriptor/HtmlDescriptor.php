844b332dd4f94834eea0c481ddf94345c4d20f6f
Alan Jeeves <runawaycoin@gmail.com>
2022-04-29 20:00:06 +0100

    Clear, refresh btns and dedupIdentifier updated
    Clear and refresh buttons added and use _stopwatch_token as dedupIdentifier

--- Command/Descriptor/HtmlDescriptor.php
+++ Command/Descriptor/HtmlDescriptor.php
@@ -14,6 +14,7 @@ namespace Symfony\Component\VarDumper\Command\Descriptor;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\VarDumper\Cloner\Data;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use function var_dump;
 
 /**
  * Describe collected data clones for html output.
@@ -37,7 +38,14 @@ class HtmlDescriptor implements DumpDescriptorInterface
         if (!$this->initialized) {
             $styles = file_get_contents(__DIR__.'/../../Resources/css/htmlDescriptor.css');
             $scripts = file_get_contents(__DIR__.'/../../Resources/js/htmlDescriptor.js');
+            $output->writeln("<html><head>");
             $output->writeln("<style>$styles</style><script>$scripts</script>");
+            $output->writeln("</head><body style='visibility: hidden'>");
+
+            $output->writeln('<div style="order: 1"><header class=""></header><section class="body"><p class="text-small">');
+            $output->writeln("<a style=' background-color: #4CAF50;  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none;  display: inline-block;  font-size: 16px;' id='clearBtn' href=''>Clear</a>&nbsp;");
+            $output->writeln("<a style=' background-color: #4CAF50;  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none;  display: inline-block;  font-size: 16px;' id='refreshBtn' href=''>Refresh</a>");
+            $output->writeln("</p></section></div>");
             $this->initialized = true;
         }
 
@@ -46,7 +54,9 @@ class HtmlDescriptor implements DumpDescriptorInterface
             $request = $context['request'];
             $controller = "<span class='dumped-tag'>{$this->dumper->dump($request['controller'], true, ['maxDepth' => 0])}</span>";
             $title = sprintf('<code>%s</code> <a href="%s">%s</a>', $request['method'], $uri = $request['uri'], $uri);
-            $dedupIdentifier = $request['identifier'];
+            // aj
+            $dedupIdentifier = $request['_stopwatch_token'];
+            //$dedupIdentifier = $request['identifier'];
         } elseif (isset($context['cli'])) {
             $title = '<code>$ </code>'.$context['cli']['command_line'];
             $dedupIdentifier = $context['cli']['identifier'];
@@ -70,7 +80,13 @@ class HtmlDescriptor implements DumpDescriptorInterface
             'project dir' => $projectDir ?? null,
         ]);
 
-        $output->writeln(<<<HTML
+        // so can merge dumps from same req and dump call
+        $outid = $dedupIdentifier . $source['name'] . $source['line'];
+
+
+
+            $output->writeln(
+                <<<HTML
 <article data-dedup-id="$dedupIdentifier">
     <header>
         <div class="row">
@@ -82,14 +98,15 @@ class HtmlDescriptor implements DumpDescriptorInterface
         {$this->renderTags($tags)}
     </header>
     <section class="body">
-        <p class="text-small">
+        <p class="text-small" data-dedup-id="$outid">
             $sourceDescription
         </p>
         {$this->dumper->dump($data, true)}
     </section>
 </article>
 HTML
-        );
+            );
+
     }
 
     private function extractDate(array $context, string $format = 'r'): string
